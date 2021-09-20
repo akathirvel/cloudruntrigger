f21efb07051e07d1bf2dd9a9e0ef4687e8948fdb
akathirvel <39790038+akathirvel@users.noreply.github.com>
2021-09-20 08:16:06 +0200

    Update 1-couldrun_build.sh

--- scripts/1-couldrun_build.sh
+++ scripts/1-couldrun_build.sh
@@ -2,7 +2,7 @@ DOCKERFILE=/workspace/src/devops/DockerfileCloudRun
 REPO=$1
 BRANCH=$2
 
-echo -e "building branch $BRANCH with env vars:\n$(cat /workspace/src/devops/_docker-build-env)"
+#echo -e "building branch $BRANCH with env vars:\n$(cat /workspace/src/devops/_docker-build-env)"
 echo "---"
 REPO2=${REPO,,}
 BRANCH2=${BRANCH,,}
